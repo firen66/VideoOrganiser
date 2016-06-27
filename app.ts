1741a1d014ab65d406d965021e642298f8eec5f1
Mehske <Mehske@von-schaewen.de>
2016-06-27 09:54:26 +0200

    Hello World + Typescript

---
+++ app.ts
@@ -0,0 +1,28 @@
+import express = require("express")
+let app = express()
+let port = process.argv[2] || 8000
+
+app.get("/", (req, res) => {
+	res.write("Hello World!")
+	res.end()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+	console.log("Server running on port: " + port)
+})
